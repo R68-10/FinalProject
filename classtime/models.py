cd8c7d2edc54960b87100a4dbadfe6b3803b6ecc
Raghed <raqed68@gmail.com>
2022-12-07 23:54:33 +0300

    project added

---
+++ classtime/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Blog1(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    date = models.DateTimeField()
+    image = models.ImageField(upload_to ="img/%y", null = True) #this
+
+    def _str_(self):
+        return self.title
